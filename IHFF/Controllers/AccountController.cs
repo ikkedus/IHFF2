122e5ca8836d2c6e032f2758a19b34ddab78d87d
matva <matva@KACHEL>
2016-11-08 20:19:28 +0100

    Authenticatie

---
+++ IHFF/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace IHFF.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginVm login) {
+            if (false)//hier in authenticatie logica stopen. XD
+            {
+                FormsAuthentication.SetAuthCookie("Username",false);
+            }
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/");
+        }
+    }
+}
