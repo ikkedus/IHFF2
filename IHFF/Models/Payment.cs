2fe9516abfb3d05e8543715ad716cde505ee5f8f
matva <matva@KACHEL>
2016-12-01 22:47:27 +0100

    vormgeving afreken pagina

---
+++ IHFF/Models/Payment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IHFF
+{
+    public class Payment
+    {
+        public string Name { get; set; }
+        public string Prefix { get; set; }
+        public string Lastname { get; set; }
+        public string PaymentOption { get; set; }
+    }
+}
