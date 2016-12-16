2b4a0a487bd38005d606761334290656762439f8
swing7wing <568507@student.inholland.nl>
2016-12-16 12:13:13 +0100

    ..

--- IHFF/Controllers/CultureController.cs
+++ IHFF/Controllers/CultureController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IHFF.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,11 @@ namespace IHFF.Controllers
 {
     public class CultureController : Controller
     {
+        private DatabaseEntities db = new DatabaseEntities();
         // GET: Culture
         public ActionResult Index()
         {
-            return View();
+            return View(db.Cultures.ToList());
         }
     }
 }
