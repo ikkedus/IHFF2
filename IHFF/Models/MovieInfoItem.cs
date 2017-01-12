f7231ef061c6e1f74e72ff0aa3d2764d5dbd8ea7
reniker <renik_nike@live.nl>
2017-01-12 12:16:10 +0100

    test

---
+++ IHFF/Models/MovieInfoItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IHFF.Models
+{
+    public class MovieInfoItem
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string banner { get; set; }
+        public string plot_EN { get; set; }
+        public string plot_NL { get; set; }
+        public string director { get; set; }
+        public string actor { get; set; }
+        public DateTime releasedate { get; set; }
+        public int duration { get; set; }
+        public string trailer { get; set; }
+        public DateTime start_Time { get; set; }
+        public DateTime end_Time { get; set; }
+        public int Eventid { get; set; }
+        public string name { get; set; }
+        public List<Times> times { get; set; }
+    }
+}
